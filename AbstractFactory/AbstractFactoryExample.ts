585612392e9444766045f73df9fcf6f426b0b854
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2020-12-12 21:03:24 +0500

    Lint: fix AbstractFactoryExample

--- AbstractFactory/AbstractFactoryExample.ts
+++ AbstractFactory/AbstractFactoryExample.ts
@@ -17,70 +17,70 @@ interface CoffeeTable {
 }
 
 class ModernFurnitureFactory implements FurnitureFactory {
-	public createChair(): Chair {
-		return new ModernChair();
-	}	
+  public createChair(): Chair {
+    return new ModernChair();
+  }	
 
-	public createCoffeeTable(): CoffeeTable {
-		return new ModernCoffeeTable();
-	}
+  public createCoffeeTable(): CoffeeTable {
+    return new ModernCoffeeTable();
+  }
 }
 
 class ModernChair implements Chair {
-	public sit(): void {
-		// ...
-	}
+  public sit(): void {
+    // ...
+  }
 }
 
 class ModernCoffeeTable implements CoffeeTable {
-	public useForCoffee() {
-		// ...
-	}
+  public useForCoffee() {
+    // ...
+  }
 }
 
 class VictorianFurnitureFactory implements FurnitureFactory {
-	public createChair(): Chair {
-		return new VictorianChair();
-	}
+  public createChair(): Chair {
+    return new VictorianChair();
+  }
 
-	public createCoffeeTable(): CoffeeTable {
-		return new VictorianCoffeeTable();
-	}
+  public createCoffeeTable(): CoffeeTable {
+    return new VictorianCoffeeTable();
+  }
 }
 
 class VictorianChair implements Chair {
-	public sit(): void {
-		// ...
-	}
+  public sit(): void {
+    // ...
+  }
 }
 
 class VictorianCoffeeTable implements CoffeeTable {
-	public useForCoffee() {
-		// ...
-	}
+  public useForCoffee() {
+    // ...
+  }
 }
 
 class Application {
 	private _factory: FurnitureFactory;
 
 	constructor(furtinureType: FurnitureType) {
-		switch (furtinureType) {
-			case FurnitureType.MODERN:
-				this._factory = new ModernFurnitureFactory();
-				break;
-			case FurnitureType.VICTORIAN:
-				this._factory = new VictorianFurnitureFactory();
-				break;
-			default:
-				throw new Error("This factory does not exist");		
-		}
+	  switch (furtinureType) {
+	    case FurnitureType.MODERN:
+	      this._factory = new ModernFurnitureFactory();
+	      break;
+	    case FurnitureType.VICTORIAN:
+	      this._factory = new VictorianFurnitureFactory();
+	      break;
+	    default:
+	      throw new Error("This factory does not exist");		
+	  }
 	}
 
 	public run() {
-		const chair = this._factory.createChair();
-		chair.sit();
-		const coffeeTable = this._factory.createCoffeeTable();
-		coffeeTable.useForCoffee();
+	  const chair = this._factory.createChair();
+	  chair.sit();
+	  const coffeeTable = this._factory.createCoffeeTable();
+	  coffeeTable.useForCoffee();
 	}
 }
 
