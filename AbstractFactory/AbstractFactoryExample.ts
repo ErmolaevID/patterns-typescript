5ffdeabbca0315558a05b7993b2dbc5a0f1575d0
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2021-01-01 15:14:24 +0500

    Create namespace for each file

--- AbstractFactory/AbstractFactoryExample.ts
+++ AbstractFactory/AbstractFactoryExample.ts
@@ -1,88 +1,90 @@
-enum FurnitureType {
-  "MODERN",
-  "VICTORIAN"
-}
-
-interface FurnitureFactory {
-  createChair(): Chair;
-  createCoffeeTable(): CoffeeTable;
-}
-
-interface Chair {
-  sit(): void;
-}
-
-interface CoffeeTable {
-  useForCoffee(): void;
-}
-
-class ModernFurnitureFactory implements FurnitureFactory {
-  public createChair(): Chair {
-    return new ModernChair();
+namespace AbstractFactoryExample {
+  enum FurnitureType {
+    "MODERN",
+    "VICTORIAN"
   }
 
-  public createCoffeeTable(): CoffeeTable {
-    return new ModernCoffeeTable();
-  }
-}
-
-class ModernChair implements Chair {
-  public sit(): void {
-    // ...
-  }
-}
-
-class ModernCoffeeTable implements CoffeeTable {
-  public useForCoffee() {
-    // ...
-  }
-}
-
-class VictorianFurnitureFactory implements FurnitureFactory {
-  public createChair(): Chair {
-    return new VictorianChair();
+  interface FurnitureFactory {
+    createChair(): Chair;
+    createCoffeeTable(): CoffeeTable;
   }
 
-  public createCoffeeTable(): CoffeeTable {
-    return new VictorianCoffeeTable();
+  interface Chair {
+    sit(): void;
   }
-}
 
-class VictorianChair implements Chair {
-  public sit(): void {
-    // ...
+  interface CoffeeTable {
+    useForCoffee(): void;
   }
-}
 
-class VictorianCoffeeTable implements CoffeeTable {
-  public useForCoffee() {
-    // ...
-  }
-}
+  class ModernFurnitureFactory implements FurnitureFactory {
+    public createChair(): Chair {
+      return new ModernChair();
+    }
 
-class Application {
-  private _factory: FurnitureFactory;
-
-  constructor(furtinureType: FurnitureType) {
-    switch (furtinureType) {
-      case FurnitureType.MODERN:
-        this._factory = new ModernFurnitureFactory();
-        break;
-      case FurnitureType.VICTORIAN:
-        this._factory = new VictorianFurnitureFactory();
-        break;
-      default:
-        throw new Error("This factory does not exist");
+    public createCoffeeTable(): CoffeeTable {
+      return new ModernCoffeeTable();
     }
   }
 
-  public run() {
-    const chair = this._factory.createChair();
-    chair.sit();
-    const coffeeTable = this._factory.createCoffeeTable();
-    coffeeTable.useForCoffee();
+  class ModernChair implements Chair {
+    public sit(): void {
+      // ...
+    }
   }
-}
 
-const app = new Application(FurnitureType.MODERN);
-app.run();
+  class ModernCoffeeTable implements CoffeeTable {
+    public useForCoffee() {
+      // ...
+    }
+  }
+
+  class VictorianFurnitureFactory implements FurnitureFactory {
+    public createChair(): Chair {
+      return new VictorianChair();
+    }
+
+    public createCoffeeTable(): CoffeeTable {
+      return new VictorianCoffeeTable();
+    }
+  }
+
+  class VictorianChair implements Chair {
+    public sit(): void {
+      // ...
+    }
+  }
+
+  class VictorianCoffeeTable implements CoffeeTable {
+    public useForCoffee() {
+      // ...
+    }
+  }
+
+  class Application {
+    private _factory: FurnitureFactory;
+
+    constructor(furtinureType: FurnitureType) {
+      switch (furtinureType) {
+        case FurnitureType.MODERN:
+          this._factory = new ModernFurnitureFactory();
+          break;
+        case FurnitureType.VICTORIAN:
+          this._factory = new VictorianFurnitureFactory();
+          break;
+        default:
+          throw new Error("This factory does not exist");
+      }
+    }
+
+    public run() {
+      const chair = this._factory.createChair();
+      chair.sit();
+      const coffeeTable = this._factory.createCoffeeTable();
+      coffeeTable.useForCoffee();
+    }
+  }
+
+  const app = new Application(FurnitureType.MODERN);
+  app.run();
+}
